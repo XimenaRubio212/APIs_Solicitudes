473efaf29c726912f6822c32ff59b82f9cc5319e
XimenaRubio212 <ximenarubio121@gmail.com>
2026-02-02 16:49:43 -0500

    Se crearon dos carpetas una llamada Apropiacion que van a ser las consultas y la otra carpeta llamada Servidor en el cual se va ejecutar el servidor JSON-Server y se realizo a actividad de apropiacion de la actividad practic de la parte 1

---
+++ Apropiacion/app1.js
@@ -0,0 +1,48 @@
+//PARTE 1: Solicitud 1.
+
+//aqui vamos a 
+const obtenerUsuario = async () => {
+    let respuesta = await fetch('http://localhost:3000/users');
+    let datos = await respuesta.json();
+    return datos;
+}
+
+//Solicitud 2
+const consultarUsuarioPorId = async (id) => {
+    let respuesta = await fetch(`http://localhost:3000/users/${id}`);
+    let datos = await respuesta.json();
+    return datos;
+}
+
+//Solicitud 3
+const obtenerPublicacionesUsuario = async (userId) => {
+    let respuesta = await fetch(`http://localhost:3000/posts?userId=${userId}`);
+    let datos = await respuesta.json();
+    return datos;
+}
+
+//Ejecutamos la solicitud 1
+const ejecutarSolicitud1 = async () => {
+    console.log(`--- Solicitud 1 ---`);
+    let usuario = await obtenerUsuario();
+    console.log(usuario);
+}
+
+//Ejecutamos la solicitud 2
+const ejecutarSolicitud2 = async (id) => {
+    console.log(`--- Solicitud 2 ---`);
+    let usuario = await consultarUsuarioPorId(id);
+    console.log(usuario);
+}
+
+//Ejecutamos la solicitud 3
+const ejecutarSolicitud3 = async (userId) => {
+    console.log(`--- Solicitud 3 ---`);
+    let publicaciones = await obtenerPublicacionesUsuario(userId);
+    console.log(publicaciones);
+}
+
+//Descomentar para ejecutar las solicitudes
+// ejecutarSolicitud1();
+// ejecutarSolicitud2(1);
+// ejecutarSolicitud3(1);
